3c84bd950cb77b46b0af88c48d078ea9b92aa96c
Zach Garwood <zachgarwood@gmail.com>
2015-04-04 18:32:38 -0500

    Created skeleton for app

---
+++ public/app.js
@@ -0,0 +1,14 @@
+var App = React.createClass({
+});
+
+var Accuracy = React.createClass({
+});
+
+var Progress = React.createClass({
+});
+
+var Question = React.createClass({
+});
+
+var Options = React.createClass({
+});
