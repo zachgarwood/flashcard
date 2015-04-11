09dedfd690fdc1b24bdb44dea6570d3f0a90f132
Zach Garwood <zachgarwood@gmail.com>
2015-04-10 23:16:25 -0500

    Added "guesses"" routes and persistence

--- bin/flashcard.js
+++ bin/flashcard.js
@@ -1,25 +1,55 @@
 const path = require('path');
 const webRoot = path.join(path.dirname(__dirname), 'public');
 const express = require('express');
+const bodyParser = require('body-parser');
 const persistence = require('../lib/persistence.js');
 
 var app = express();
 app.use(express.static(webRoot));
+app.use(bodyParser());
 app.get('/questions', function(request, response) {
-  console.log('questions');
+  console.log('get questions');
   persistence.client.connect(persistence.url, function(error, db) {
     if (error) {
       return console.dir(error);
     }
-    var questions = db.collection('questions');
-    questions.find().toArray(function(error, items) {
-      response.send(items);
-      db.close();
-    });
+    db.collection('questions')
+      .find().toArray(function(error, items) {
+        if (error) {
+          return console.dir(error);
+        }
+        response.send(items);
+        db.close();
+      });
   });
 });
 app.get('/guesses', function(request, response) {
-  console.log('guesses');
-  response.send(require('./guesses.json'));
+  console.log('get guesses');
+  persistence.client.connect(persistence.url, function(error, db) {
+    if (error) {
+      return console.dir(error);
+    }
+    db.collection('guesses')
+      .find().toArray(function(error, items) {
+        if (error) {
+          return console.dir(error);
+        }
+        response.send(items);
+        db.close
+      })
+  });
+});
+app.post('/guesses', function(request, response) {
+  console.log('post guesses');
+  persistence.client.connect(persistence.url, function(error, db) {
+    if (error) {
+      return console.dir(error);
+    }
+    db.collection('guesses')
+      .insert(request.body);
+    response.status(200).end();
+    db.close
+  });
 });
 app.listen(8080);
+console.log('listening');
